67ee4a7f79c45ab35485590a63a0a78c61b45c3f
bangbang93 <bangbang93@163.com>
2024-06-27 11:52:29 +0800

    fix: 应该使用verifyIntelligentCaptcha

--- src/aliyun-captcha.service.ts
+++ src/aliyun-captcha.service.ts
@@ -1,4 +1,4 @@
-import Client, {VerifyCaptchaRequest} from '@alicloud/captcha20230305'
+import Client, {VerifyIntelligentCaptchaRequest} from '@alicloud/captcha20230305'
 import {Config} from '@alicloud/openapi-client'
 import {Inject, Injectable} from '@nestjs/common'
 import {AliyunCaptchaOptions, MODULE_OPTIONS_TOKEN} from './aliyun-captcha-configure.module'
@@ -13,15 +13,15 @@ export class AliyunCaptchaService {
 
   public async verify(params: string, sceneId?: string): Promise<boolean> {
     const client = new Client(this.config)
-    const request = new VerifyCaptchaRequest({
+    const request = new VerifyIntelligentCaptchaRequest({
       sceneId: sceneId ?? this.options.sceneId,
     })
     request.captchaVerifyParam = params
-    const res = await client.verifyCaptcha(request)
+    const res = await client.verifyIntelligentCaptcha(request)
     if (res.body.result?.verifyResult) {
       return true
     }
-    const verifyCode = res.body.result?.verifyCode as string
+    const verifyCode = res.body.result?.verifyCode
     if (verifyCode) {
       throw new CaptchaError(verifyCode)
     }
